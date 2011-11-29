5bc8ca50e0be88ec0f0dd69b150bfa339dbdb903
Kostas Zervakis <zervakis@silicor.gr>
2011-11-30 00:58:47 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAap::Application.config.secret_token = '3458f92410b28baa0eb08882acf43a15c167123628edd6230eec420fea355ed06f52e8c0c4dd14e554e625ee722312d213701d3dadcfc8bf2cedab1d7339e0c1'
